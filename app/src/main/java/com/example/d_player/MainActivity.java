76101897cc692ca58e05b872e13a3cb4c8ff7566
justdvnsh <justdvnsh2208@gmail.com>
2019-09-24 00:48:12 +0530

    added seekbar and timer

--- app/src/main/java/com/example/d_player/MainActivity.java
+++ app/src/main/java/com/example/d_player/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.d_player;
 
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
@@ -10,19 +11,53 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.widget.VideoView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
-//    Button playPause = findViewById(R.id.play);
+    MediaPlayer mediaPlayer; // Creating a mediaPlayer Variable, which we will use in the onCreate method.
+    String totalDuration; // Creatng a total Duration method.
+    Handler timerHandler = new Handler(); // making a new HandlerObject to handle time.
 
+    // Creating a function to make milliseconds into minutes and seconds.
+    public String milliSecondsToTimer(int duration) {
+
+        long minutes = ( duration / 1000 ) / 60;
+        long seconds = ( duration / 1000 ) % 60;
+
+        if ( minutes < 10 & seconds < 10) {
+
+            return "0" + minutes + ":" + "0" + seconds;
+
+        } else if ( minutes < 10 & seconds >= 10) {
+
+            return "0" + minutes + ":" + seconds;
+
+        } else if ( minutes >= 10 & seconds < 10 ) {
+
+            return minutes + ":" + "0" + seconds;
+
+        } else {
+
+            return minutes + ":" + seconds;
+
+        }
+
+    };
+
+    // onClick method for 
     public void playMusic(View view) {
 
        Button playPause = findViewById(R.id.play);
@@ -30,12 +65,28 @@ public class MainActivity extends AppCompatActivity {
        if (!mediaPlayer.isPlaying()) {
            mediaPlayer.start();
            playPause.setText(R.string.pause);
+           timerHandler.postDelayed(updateTimer, 0);
        } else {
            mediaPlayer.pause();
            playPause.setText(R.string.play);
+           timerHandler.removeCallbacks(updateTimer);
        }
 
-    }
+    };
+
+    private Runnable updateTimer = new Runnable() {
+
+        @Override
+        public void run() {
+
+            int currentDuration = mediaPlayer.getCurrentPosition();
+
+            TextView currentDurationText = (TextView) findViewById(R.id.currentDuration);
+            currentDurationText.setText(milliSecondsToTimer(currentDuration));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +95,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+//        TextView currentDurationText = ( TextView ) findViewById(R.id.currentDuration);
         mediaPlayer = MediaPlayer.create(this, R.raw.fast_foot);
+        SeekBar seekbar = ( SeekBar ) findViewById(R.id.seekBar);
+        seekbar.setMax(mediaPlayer.getDuration());
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekbar){}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                Log.i("Seek bar Progress", Integer.toString(progress));
+
+            }
+
+
+
+        });
+
+        totalDuration = milliSecondsToTimer(mediaPlayer.getDuration());
+
+        TextView duration = ( TextView ) findViewById(R.id.duration);
+        Log.i("Duration", totalDuration);
+        duration.setText(totalDuration);
     }
 
     @Override
