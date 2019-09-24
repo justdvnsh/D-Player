60d19af0901404f914c17278a5484df724b42048
justdvnsh <justdvnsh2208@gmail.com>
2019-09-24 16:30:24 +0530

    added storage files

--- app/src/main/java/com/example/d_player/MainActivity.java
+++ app/src/main/java/com/example/d_player/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.d_player;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.d_player.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.ScrollView;
+import android.widget.Scroller;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +48,9 @@ import org.w3c.dom.Text;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -38,6 +59,28 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer; // Creating a mediaPlayer Variable, which we will use in the onCreate method.
     String totalDuration; // Creatng a total Duration method.
     Handler timerHandler = new Handler(); // making a new HandlerObject to handle time.
+
+    Context context;
+
+    public static final int RUNTIME_PERMISSION_CODE = 7;
+
+    String[] ListElements = new String[] { };
+
+    ListView listView;
+
+    List<String> ListElementsArrayList ;
+
+    ArrayAdapter<String> adapter ;
+
+    ContentResolver contentResolver;
+
+    Cursor cursor;
+
+    Uri uri;
+
+    // Makeing the file variable and Array containing the files.
+    private File root;
+    private ArrayList<File> fileList = new ArrayList<File>();
 
     // Creating a function to make milliseconds into minutes and seconds.
     public String milliSecondsToTimer(int duration) {
@@ -105,34 +148,113 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public void GetAllMediaMp3Files(){
 
+        contentResolver = context.getContentResolver();
 
-    private ArrayList<HashMap<String,String>> getPlayList(String rootPath) {
-        ArrayList<HashMap<String,String>> fileList = new ArrayList<>();
+        uri = Uri.parse("internal/storage/self");
 
+        cursor = contentResolver.query(
+                uri, // Uri
+                null,
+                null,
+                null,
+                null
+        );
 
-        try {
-            File rootFolder = new File(rootPath);
-            File[] files = rootFolder.listFiles(); //here you will get NPE if directory doesn't contains  any file,handle it like this.
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    if (getPlayList(file.getAbsolutePath()) != null) {
-                        fileList.addAll(getPlayList(file.getAbsolutePath()));
-                    } else {
-                        break;
-                    }
-                } else if (file.getName().endsWith(".mp3")) {
-                    HashMap<String, String> song = new HashMap<>();
-                    song.put("file_path", file.getAbsolutePath());
-                    song.put("file_name", file.getName());
-                    fileList.add(song);
+        if (cursor == null) {
+
+            Toast.makeText(MainActivity.this,"Something Went Wrong.", Toast.LENGTH_LONG);
+
+        } else if (!cursor.moveToFirst()) {
+
+            Toast.makeText(MainActivity.this,"No Music Found on SD Card.", Toast.LENGTH_LONG);
+
+        }
+        else {
+
+            int Title = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+
+            //Getting Song ID From Cursor.
+            //int id = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+
+            do {
+
+                // You can also get the Song ID using cursor.getLong(id).
+                //long SongID = cursor.getLong(id);
+
+                String SongTitle = cursor.getString(Title);
+
+                // Adding Media File Names to ListElementsArrayList.
+                ListElementsArrayList.add(SongTitle);
+
+            } while (cursor.moveToNext());
+        }
+    }
+
+    // Creating Runtime permission function.
+    public void AndroidRuntimePermission(){
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                    AlertDialog.Builder alert_builder = new AlertDialog.Builder(MainActivity.this);
+                    alert_builder.setMessage("External Storage Permission is Required.");
+                    alert_builder.setTitle("Please Grant Permission.");
+                    alert_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            ActivityCompat.requestPermissions(
+                                    MainActivity.this,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    RUNTIME_PERMISSION_CODE
+
+                            );
+                        }
+                    });
+
+                    alert_builder.setNeutralButton("Cancel",null);
+
+                    AlertDialog dialog = alert_builder.create();
+
+                    dialog.show();
+
                 }
+                else {
+
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            RUNTIME_PERMISSION_CODE
+                    );
+                }
+            }else {
+
             }
-            return fileList;
-        } catch (Exception e) {
-            return null;
         }
     };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        switch(requestCode){
+
+            case RUNTIME_PERMISSION_CODE:{
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }
+                else {
+
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -141,16 +263,43 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // getting the scrollview
+        ScrollView scroll = ( ScrollView ) findViewById(R.id.scroll);
+        listView = (ListView) findViewById(R.id.listview1);
+
         // getting all the media files from the phone
-        ArrayList<HashMap<String,String>> songList=getPlayList(Environment.getExternalStorageDirectory().getAbsolutePath());
-        if(songList!=null){
-            for(int i=0;i<songList.size();i++){
-                String fileName=songList.get(i).get("file_name");
-                String filePath=songList.get(i).get("file_path");
-                //here you will get list of file name and file path that present in your device
-                Log.i("file details "," name ="+fileName +" path = "+filePath);
-               }
+        context = getApplicationContext();
+
+        ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
+
+        Log.i("MP3 FILES FOUND ARE", ListElementsArrayList.toString());
+
+        adapter = new ArrayAdapter<String>
+                (MainActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+
+        // Requesting run time permission for Read External Storage.
+        AndroidRuntimePermission();
+
+
+        GetAllMediaMp3Files();
+
+        listView.setAdapter(adapter);
+
+        // ListView on item selected listener.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // TODO Auto-generated method stub
+                // Showing ListView Item Click Value using Toast.
+
+                Toast.makeText(MainActivity.this,parent.getAdapter().getItem(position).toString(),Toast.LENGTH_LONG).show();
+
             }
+        });
+
+
 
         // create a media player object.
         mediaPlayer = MediaPlayer.create(this, R.raw.fast_foot);
